1475a23a4d8cd844d1b47d11d06388de8c29b4be
Avery-Dunn <avdunn@microsoft.com>
2020-06-17 14:40:39 -0700

    Restructure, better demonstrate acquireTokenSilently call

--- Username-Password-Flow/src/main/java/UsernamePasswordFlow.java
+++ Username-Password-Flow/src/main/java/UsernamePasswordFlow.java
@@ -9,38 +9,23 @@ import com.microsoft.aad.msal4j.SilentParameters;
 import com.microsoft.aad.msal4j.UserNamePasswordParameters;
 
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.Collections;
 import java.util.Properties;
 import java.util.Set;
 
 public class UsernamePasswordFlow {
 
+    private static String authority;
+    private static Set<String> scope;
+    private static String clientId;
+    private static String username;
+    private static String password;
+    private static String accounts;
+
     public static void main(String args[]) throws Exception {
 
-        IAuthenticationResult result = acquireTokenUsernamePassword();
-        System.out.println("Access token: " + result.accessToken());
-        System.out.println("Id token: " + result.idToken());
-        System.out.println("Account username: " + result.account().username());
-    }
-
-    private static IAuthenticationResult acquireTokenUsernamePassword() throws Exception {
-        //Load properties file and set properties used throughout the sample
-        Properties properties = new Properties();
-        properties.load(new FileInputStream(Thread.currentThread().getContextClassLoader().getResource("").getPath() + "application.properties"));
-        String authority = properties.getProperty("AUTHORITY");
-        Set<String> scope = Collections.singleton(properties.getProperty("SCOPE"));
-        String clientId = properties.getProperty("CLIENT_ID");
-        String userName = properties.getProperty("USER_NAME");
-        String password = properties.getProperty("USER_PASSWORD");
-
-        String accounts = "{\"Account\": "
-                + "{\"uid.utid-login.windows.net-contoso\": "
-                + "{\"username\": \"John Doe\","
-                + "\"local_account_id\": \"object1234\","
-                + "\"realm\": \"contoso\","
-                + "\"environment\": \"login.windows.net\","
-                + "\"home_account_id\": \"uid.utid\","
-                + "\"authority_type\": \"MSSTS\"}}}";
+        setUpSampleData();
 
         TokenCacheAspect tokenCacheAspect = new TokenCacheAspect(accounts);
 
@@ -50,34 +35,94 @@ public class UsernamePasswordFlow {
                 .build();
 
         Set<IAccount> accountsInCache = pca.getAccounts().join();
-        // Take first account in the cache. In a production application, you would filter
-        // accountsInCache to get the right account for the user authenticating.
-        IAccount account = accountsInCache.iterator().next();
+        IAccount account = getAccountByUsername(accountsInCache, username);
 
+        //Attempt to acquire token when user's account is not in the application's token cache
+        IAuthenticationResult result = acquireTokenUsernamePassword(pca, scope, account, username, password);
+        System.out.println("Account username: " + result.account().username());
+        System.out.println("Access token:     " + result.accessToken());
+        System.out.println("Id token:         " + result.idToken());
+        System.out.println();
+
+        accountsInCache = pca.getAccounts().join();
+        account = getAccountByUsername(accountsInCache, username);
+
+        //Attempt to acquire token again, now that the user's account and a token are in the application's token cache
+        result = acquireTokenUsernamePassword(pca, scope, account, username, password);
+        System.out.println("Account username: " + result.account().username());
+        System.out.println("Access token:     " + result.accessToken());
+        System.out.println("Id token:         " + result.idToken());
+    }
+
+    private static IAuthenticationResult acquireTokenUsernamePassword(PublicClientApplication pca,
+                                                                      Set<String> scope,
+                                                                      IAccount account,
+                                                                      String username,
+                                                                      String password) throws Exception {
         IAuthenticationResult result;
         try {
             SilentParameters silentParameters =
                     SilentParameters
-                            .builder(scope, account)
+                            .builder(scope)
+                            .account(account)
                             .build();
-            // try to acquire token silently. This call will fail since the token cache
-            // does not have any data for the user you are trying to acquire a token for
+            // Try to acquire token silently. This will fail on the first acquireTokenUsernamePassword() call
+            // because the token cache does not have any data for the user you are trying to acquire a token for
             result = pca.acquireTokenSilently(silentParameters).join();
+            System.out.println("==acquireTokenSilently call succeeded");
         } catch (Exception ex) {
             if (ex.getCause() instanceof MsalException) {
-
+                System.out.println("==acquireTokenSilently call failed: " + ex.getCause());
                 UserNamePasswordParameters parameters =
                         UserNamePasswordParameters
-                                .builder(scope, userName, password.toCharArray())
+                                .builder(scope, username, password.toCharArray())
                                 .build();
                 // Try to acquire a token via username/password. If successful, you should see
                 // the token and account information printed out to console
                 result = pca.acquireToken(parameters).join();
+                System.out.println("==username/password flow succeeded");
             } else {
                 // Handle other exceptions accordingly
                 throw ex;
             }
         }
         return result;
+    }
+
+    /**
+     * Helper function to return an account from a given set of accounts based on the given username,
+     * or return null if no accounts in the set match
+     */
+    private static IAccount getAccountByUsername(Set<IAccount> accounts, String username) {
+        for (IAccount account : accounts) {
+            if (account.username().equals(username)) {
+                return account;
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Helper function unique to this sample setting. In a real application these wouldn't be so hardcoded, for example
+     * values such as username/password would come from the user, and different users may require different scopes
+     */
+    private static void setUpSampleData() throws IOException {
+        // Load properties file and set properties used throughout the sample
+        Properties properties = new Properties();
+        properties.load(new FileInputStream(Thread.currentThread().getContextClassLoader().getResource("").getPath() + "application.properties"));
+        authority = properties.getProperty("AUTHORITY");
+        scope = Collections.singleton(properties.getProperty("SCOPE"));
+        clientId = properties.getProperty("CLIENT_ID");
+        username = properties.getProperty("USER_NAME");
+        password = properties.getProperty("USER_PASSWORD");
+        //Junk account used to pre-fill cache
+        accounts = "{\"Account\": "
+                + "{\"uid.utid-login.windows.net-contoso\": "
+                + "{\"username\": \"John Doe\","
+                + "\"local_account_id\": \"object1234\","
+                + "\"realm\": \"contoso\","
+                + "\"environment\": \"login.windows.net\","
+                + "\"home_account_id\": \"uid.utid\","
+                + "\"authority_type\": \"MSSTS\"}}}";
     }
 }
